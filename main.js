78563278bf3ef80ed2941fe5215c8369a043f8a1
Maria Castañeda Laris <macastan@microsoft.com>
2018-07-23 11:59:58 -0700

    adding php

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 ﻿var Main = function() {
     var myParam = location.search.split('myParam=')[1];
     var urlParams = myParam.split('&');
-    console.log(urlParams);
+    if (urlParams) {
+        urlParams = urlParamns[0].replace(/\s+/g, '');
+        // Create request to Bing Translator API
+        var xhttp = new XMLHttpRequest();
+        var url = 
+        xhttp.open("POST", url, true);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.send();
+        var response = JSON.parse(xhttp.responseText);        
+    }
 }
 
-Main();
+//Main();
